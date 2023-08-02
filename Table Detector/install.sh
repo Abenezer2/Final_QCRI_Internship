2d9b6aea98a4ff0458c1eb1276dd6d86b9731a80
Abenezer2 <abenezer3294@gmail.com>
2023-08-02 16:34:04 +0300

    final

---
+++ Table Detector/install.sh
@@ -0,0 +1,6 @@
+pip install "paddleocr>=2.0.1"
+pip install protobuf==3.20.0
+git clone https://github.com/PaddlePaddle/PaddleOCR.git
+pip install paddlepaddle -f https://www.paddlepaddle.org.cn/whl/linux/mkl/avx/stable.html
+wget https://paddleocr.bj.bcebos.com/whl/layoutparser-0.0.0-py3-none-any.whl
+pip install -U layoutparser-0.0.0-py3-none-any.whl
